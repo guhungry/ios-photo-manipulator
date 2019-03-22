804cc7bea39365ce41c15c83d5525048840485e9
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-22 19:09:29 +0700

    [Bitmap Utils] Change: Add crop(), resize() and cropAndResize()

---
+++ WCPhotoManipulator/WCPhotoManipulator/ResizeMode.swift
@@ -0,0 +1,15 @@
+//
+//  ResizeMode.swift
+//  WCPhotoManipulator
+//
+//  Created by Woraphot Chokratanasombat on 22/3/2562 BE.
+//  Copyright © 2562 Woraphot Chokratanasombat. All rights reserved.
+//
+
+import UIKit
+
+enum ResizeMode: String {
+    case contain = "contain"
+    case cover = "cover"
+    case stretch = "stretch"
+}
---
+++ WCPhotoManipulator/WCPhotoManipulator/WCPhotoManipulator.h
@@ -0,0 +1,19 @@
+//
+//  WCPhotoManipulator.h
+//  WCPhotoManipulator
+//
+//  Created by Woraphot Chokratanasombat on 22/3/2562 BE.
+//  Copyright © 2562 Woraphot Chokratanasombat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WCPhotoManipulator.
+FOUNDATION_EXPORT double WCPhotoManipulatorVersionNumber;
+
+//! Project version string for WCPhotoManipulator.
+FOUNDATION_EXPORT const unsigned char WCPhotoManipulatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCPhotoManipulator/PublicHeader.h>
+
+
