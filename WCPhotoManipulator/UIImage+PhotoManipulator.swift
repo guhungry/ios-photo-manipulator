94a313b750308562802f7d22c60257bafc9735d1
Woraphot Chokratanasombat <guhungry@gmail.com>
2019-03-23 14:10:41 +0700

    [Photo Manipulator] Change: Update method signature

--- WCPhotoManipulator/UIImage+PhotoManipulator.swift
+++ WCPhotoManipulator/UIImage+PhotoManipulator.swift
@@ -8,54 +8,57 @@
 
 import UIKit
 
-public extension UIImage {
+@objc public extension UIImage {
     /**
      Crop image
-     
+
      - Parameter cropRegion: Position and Size of Image to crop
-     
+
      - Returns: Cropped image
      */
-    public func crop(_ cropRegion: CGRect) -> UIImage? {
-        let targetSize = cropRegion.size;
-        let targetRect = CGRect(x: -cropRegion.origin.x, y: -cropRegion.origin.y, width: self.size.width, height: self.size.width)
+    @objc public func crop(region: CGRect) -> UIImage? {
+        let targetRect = CGRect(x: -region.origin.x, y: -region.origin.y, width: self.size.width, height: self.size.width)
         let operation = BitmapUtils.transformFromTargetRect(self.size, targetRect)
-        
-        return BitmapUtils.transform(self, targetSize, self.scale, operation)
+
+        return BitmapUtils.transform(self, region.size, self.scale, operation)
     }
-    
+
     /**
      Resize image to targetSize
-     
+
      - Parameter targetSize: Target Image Size
      - Parameter scale: Scale of result image
-     
+
      - Returns: Resized image
      */
-    public func resize(_ targetSize: CGSize, _ scale: CGFloat? = nil) -> UIImage? {
+    @objc public func resize(targetSize: CGSize, scale: CGFloat) -> UIImage? {
         let rect = BitmapUtils.targetRect(self.size, targetSize, 1, .cover)
         let operation = BitmapUtils.transformFromTargetRect(self.size, rect)
-        return BitmapUtils.transform(self, targetSize, scale ?? self.scale, operation)
+        return BitmapUtils.transform(self, targetSize, scale, operation)
     }
-    
+
+    @objc public func resize(targetSize: CGSize) -> UIImage? {
+        return resize(targetSize: targetSize, scale: self.scale)
+    }
+
     /**
      Crop and resize image
-     
+
      - Parameter cropRegion: Position and Size of Image to crop
      - Parameter targetSize: Target Image Size
-     
+
      - Returns: Cropped and resized image
      */
-    func crop(_ cropRegion: CGRect, _ targetSize: CGSize) -> UIImage? {
-        return self.crop(cropRegion)?.resize(targetSize, self.scale)
+    @objc func crop(region: CGRect, targetSize: CGSize) -> UIImage? {
+        return self.crop(region: region)?.resize(targetSize: targetSize, scale: self.scale)
     }
-    
-    public func drawText(_ text: NSString, position: CGPoint, color: UIColor, size: CGFloat, thickness: CGFloat = 0, scale: CGFloat? = nil) -> UIImage? {
+
+    @objc public func drawText(_ text: NSString, position: CGPoint, color: UIColor, size: CGFloat, thickness: CGFloat, scale: CGFloat) -> UIImage? {
         let opaque = !self.hasAlpha()
-        UIGraphicsBeginImageContextWithOptions(self.size, opaque, scale ?? self.scale)
-        
+        UIGraphicsBeginImageContextWithOptions(self.size, opaque, scale)
+
         self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        
+
         var textStyles = [
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: size),
             NSAttributedString.Key.foregroundColor: color,
@@ -65,29 +68,37 @@ public extension UIImage {
             textStyles[NSAttributedString.Key.strokeWidth] = thickness
         }
         text.draw(at: position, withAttributes: textStyles)
-        
+
         let result = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext()
 
         return result
     }
-    
-    public func overlayImage(_ overlay: UIImage, position: CGPoint, scale: CGFloat? = nil) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(self.size, true, scale ?? self.scale)
-        
+
+    @objc public func drawText(_ text: NSString, position: CGPoint, color: UIColor, size: CGFloat, thickness: CGFloat) -> UIImage? {
+        return drawText(text, position: position, color: color, size: size, thickness: thickness, scale: self.scale)
+    }
+
+    @objc public func overlayImage(_ overlay: UIImage, position: CGPoint, scale: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.size, true, scale)
+
         self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        
+
         overlay.draw(in: CGRect(origin: position, size: overlay.size))
-        
+
         let result = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext()
-        
+
         return result
     }
-    
+
+    @objc public func overlayImage(_ overlay: UIImage, position: CGPoint) -> UIImage? {
+        return overlayImage(overlay, position: position, scale: self.scale)
+    }
+
     func hasAlpha() -> Bool {
         guard let image = self.cgImage else { return false }
-        
+
         switch (image.alphaInfo) {
         case .none, .noneSkipLast, .noneSkipFirst:
             return false
