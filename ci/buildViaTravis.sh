607e57a5dcf7017e72b27363ddb4ebe819e32404
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-27 19:14:07 +0700

    [Travis] Change: Disable iOS build

--- ci/buildViaTravis.sh
+++ ci/buildViaTravis.sh
@@ -4,18 +4,18 @@
 if [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   set -o pipefail
-  xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
+  #xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
 elif [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Tag for Release =>   Tag ['$TRAVIS_TAG']'
   set -o pipefail
-  xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
+  #xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
 
   pod spec lint
   pod trunk push
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   set -o pipefail
-  xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
+  #xcodebuild clean build test -project WCPhotoManipulator.xcodeproj -scheme WCPhotoManipulator -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.4" -configuration Debug GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -c
 fi
 
-./xccov-to-sonarqube-generic.sh ~/Library/Developer/Xcode/DerivedData/*/Logs/Test/*/*/*.xccovarchive/ > sonarqube-generic-coverage.xml
+#./xccov-to-sonarqube-generic.sh ~/Library/Developer/Xcode/DerivedData/*/Logs/Test/*/*/*.xccovarchive/ > sonarqube-generic-coverage.xml
