29fc54c77170449e11cbb3e34786dc3c3f488b62
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-04-24 18:59:19 +0700

    [Test] Change: Add blank unit test

---
+++ WCPhotoManipulatorTests/WCPhotoManipulatorTests.m
@@ -0,0 +1,37 @@
+//
+//  WCPhotoManipulatorTests.m
+//  WCPhotoManipulatorTests
+//
+//  Created by Woraphot Chokratanasombat on 24/4/2562 BE.
+//  Copyright © 2562 Woraphot Chokratanasombat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WCPhotoManipulatorTests : XCTestCase
+
+@end
+
+@implementation WCPhotoManipulatorTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
