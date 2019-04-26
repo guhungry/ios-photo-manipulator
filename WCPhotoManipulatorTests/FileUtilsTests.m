3a9dad291cc1adf79bf70a2cff981eeb065e3519
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-04-26 16:29:50 +0700

    [FileUtilsTests] Fix: Add test for imageToData()

--- WCPhotoManipulatorTests/FileUtilsTests.m
+++ WCPhotoManipulatorTests/FileUtilsTests.m
@@ -13,6 +13,12 @@
 
 @end
 
+@interface FileUtils (Private)
+
++ (NSData *)imageToData:(UIImage *)image mimeType:(NSString *)mimeType quality:(CGFloat)quality;
+
+@end
+
 @implementation FileUtilsTests
 
 - (void)setUp {
@@ -25,6 +31,8 @@
     NSString *path = [FileUtils cachePath];
     
     XCTAssertNotNil(path);
+    
+    path = nil;
 }
 
 - (void)testCreateTempFile_ShouldReturnFileWithPrefix {
@@ -36,6 +44,51 @@
     XCTAssertTrue([path hasPrefix:[FileUtils cachePath]]);
     XCTAssertTrue([name hasPrefix:prefix]);
     XCTAssertTrue([name hasSuffix:@".png"]);
+    
+    prefix = nil;
+    path = nil;
+    name = nil;
+}
+
+- (void)testImageToData_WhenOuptutJpeg_ShouldReturnJpeg {
+    UIImage *image = [UIImage imageNamed:@"overlay" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
+    NSData *data = [FileUtils imageToData:image mimeType:@"image/jpeg" quality:100];
+    
+    XCTAssertNotNil(data);
+    XCTAssertEqual([self imageMimeType:data], @"image/jpeg");
+    
+    image = nil;
+    data = nil;
+}
+
+- (void)testImageToData_WhenOuptutJpeg_ShouldReturnPng {
+    UIImage *image = [UIImage imageNamed:@"overlay" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
+    NSData *data = [FileUtils imageToData:image mimeType:@"image/png" quality:100];
+    
+    XCTAssertNotNil(data);
+    XCTAssertEqual([self imageMimeType:data], @"image/png");
+    
+    image = nil;
+    data = nil;
+}
+
+- (NSString *)imageMimeType:(NSData *)data {
+    uint8_t c;
+    [data getBytes:&c length:1];
+    
+    switch (c) {
+        case 0xFF:
+            return @"image/jpeg";
+        case 0x89:
+            return @"image/png";
+        case 0x47:
+            return @"image/gif";
+        case 0x49:
+        case 0x4D:
+            return @"image/tiff";
+    }
+    return nil;
 }
 
 @end
+
