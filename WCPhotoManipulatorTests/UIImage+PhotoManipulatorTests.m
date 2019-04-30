031b35a1c6906c338480787f186380f075655425
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-04-30 11:06:22 +0700

    [UIImage PhotoManipulator] Change: Add test for cropAndResize()

--- WCPhotoManipulatorTests/UIImage+PhotoManipulatorTests.m
+++ WCPhotoManipulatorTests/UIImage+PhotoManipulatorTests.m
@@ -60,4 +60,17 @@
     XCTAssertEqual(image.size.height, 99);
 }
 
+- (void)testCropAndResize_ShouldReturnSizeCorrectly {
+    image = [UIImage imageNamedTest:@"background.jpg"];
+    UIColor *expectedColor = [image colorAt:CGPointMake(61, 144)];
+    XCTAssertNotNil(image);
+    
+    image = [image crop:CGRectMake(61, 144, 166, 35) targetSize:CGSizeMake(332, 70)];
+    UIColor *actualColor = [image colorAt:CGPointMake(0, 0)];
+    XCTAssertNotNil(image);
+    XCTAssertEqual(image.size.width, 332);
+    XCTAssertEqual(image.size.height, 70);
+    XCTAssertTrue([actualColor isEqual:expectedColor]);
+}
+
 @end
