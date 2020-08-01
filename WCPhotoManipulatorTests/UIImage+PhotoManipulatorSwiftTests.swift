7dead9dd0848c0f4f8bf74a93d14b122512c6ea6
Woraphot Chokratanasombat <guhungry@gmail.com>
2020-08-01 23:49:16 +0700

    [UIImage+PhotoManipulatorSwiftTests] Change: Add test for UIImage+PhotoManipulator in Swift

---
+++ WCPhotoManipulatorTests/UIImage+PhotoManipulatorSwiftTests.swift
@@ -0,0 +1,108 @@
+//
+//  UIImage+PhotoManipulatorSwiftTests.swift
+//  WCPhotoManipulatorTests
+//
+//  Created by Woraphot Chokratanasombat on 1/8/20.
+//  Copyright © 2020 Woraphot Chokratanasombat. All rights reserved.
+//
+
+import XCTest
+
+class UIImage_PhotoManipulatorSwiftTests: XCTestCase {
+    var image: UIImage! = nil
+    var overlay: UIImage! = nil
+    
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+        image = nil
+        overlay = nil
+    }
+
+    func testHasAlpha_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "overlay.png")
+        XCTAssertNotNil(image)
+        XCTAssertTrue(image.responds(to: Selector(("hasAlpha"))))
+        XCTAssertTrue(image.hasAlpha())
+    
+        
+        image = UIImage.init(namedTest: "background.jpg")
+        XCTAssertNotNil(image)
+        XCTAssertFalse(image.hasAlpha())
+    }
+
+    func testCrop_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        let expectedColor = image.color(at: CGPoint(x: 52, y: 89))
+        XCTAssertNotNil(image)
+    
+    
+        image = image.crop(CGRect(x: 52, y: 89, width: 65, height: 74))
+        let actualColor = image.color(at: CGPoint(x: 0, y: 0))
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 65, height: 74))
+        XCTAssertEqual(actualColor, expectedColor)
+    }
+
+    func testResize_ShouldReturnSizeCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        XCTAssertNotNil(image)
+    
+    
+        image = image.resize(CGSize(width: 55, height: 99), scale: 1)
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 55, height: 99))
+    }
+
+    func testCropAndResize_ShouldReturnSizeCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        let expectedColor = image.color(at: CGPoint(x: 61, y: 144))
+        XCTAssertNotNil(image)
+    
+    
+        image = image.crop(CGRect(x: 61, y: 144, width: 166, height: 35), targetSize: CGSize(width: 332, height: 70))
+        let actualColor = image.color(at: CGPoint(x: 0, y: 0))
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 332, height: 70))
+        XCTAssertEqual(actualColor, expectedColor)
+    }
+
+    func testDrawText_WhenNoScale_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        XCTAssertNotNil(image)
+
+        image = image.drawText("Test Text To Draw", position: CGPoint(x: 15, y: 66), color: .blue, size: 42, thickness: 5)
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 800, height: 530))
+    }
+
+    func testDrawText_WhenHasScale_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        XCTAssertNotNil(image)
+
+        image = image.drawText("Test Text To Draw", position: CGPoint(x: 15, y: 66), color: .blue, size: 42, thickness: 5, scale: 2)
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 800, height: 530))
+    }
+
+    func testOverlayImage_WhenNoScale_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        overlay = UIImage.init(namedTest: "overlay.png")
+        XCTAssertNotNil(image)
+    
+        image = image.overlayImage(overlay, position: CGPoint(x: 66, y: 115))
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 800, height: 530))
+    }
+
+    func testOverlayImage_WhenHasScale_ShouldReturnCorrectly() throws {
+        image = UIImage.init(namedTest: "background.jpg")
+        overlay = UIImage.init(namedTest: "overlay.png")
+        XCTAssertNotNil(image)
+    
+        image = image.overlayImage(overlay, position: CGPoint(x: 66, y: 115), scale: 3)
+        XCTAssertNotNil(image)
+        XCTAssertEqual(image.size, CGSize(width: 800, height: 530))
+    }
+}
