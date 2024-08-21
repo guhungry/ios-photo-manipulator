a2750dfc9a4ebf9708331c556e1e777a7b7849d7
Woraphot Chokratanasombat <guhungry@gmail.com>
2024-08-21 21:34:06 +0700

    [Release] Change: Bump to 2.4.2
    Lower platforms to ios9 like podspec

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WCPhotoManipulator",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "WCPhotoManipulator",
